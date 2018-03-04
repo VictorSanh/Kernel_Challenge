8cfb601af1b5b31027b3c938579dd4a874b4f722
VictorSanh <victorsanh@gmail.com>
2018-03-04 18:30:33 +0100

    Spectrum Kernel

--- kernels.py
+++ kernels.py
@@ -2,6 +2,9 @@
 import numpy as np
 from numpy.core.defchararray import not_equal
 
+
+###################
+##### HAMMING #####
 def hamming_distance(source, target):
     """Return the Hamming distance between equal-length sequences"""
     return np.count_nonzero(not_equal(source,target))
@@ -14,6 +17,9 @@ def hamming_kernel(source, target):
         raise ValueError("Strings are empty")
     return 1 - (hamming_distance(source, target)*1.0/len(source))
 
+
+#######################
+##### LEVENSHTEIN #####
 def levenshtein_distance(source, target):
     if len(source) < len(target):
         return levenshtein(target, source)
@@ -58,3 +64,75 @@ def levenshtein_kernel(source, target):
     if len(source) == 0:
         raise ValueError("Strings are empty")
     return 1 - (levenshtein_distance(source, target)*1.0/len(source))
+
+
+####################
+##### SPECTRUM #####
+def create_dictionary(training_sequences, substring_length):
+    '''Create the dictionary/vocabulary of possible subsequeces of length substring_length from training sequences.
+    "ABCD" contains two words of legnth 3: "ABC" and "BCD".
+    
+    Input:
+        training_sequences: array like strucutre containing training sequences
+        substring_length: length of substring in sequence
+    Output:
+        word_2_index: mapping between a word and its index. The keys are all the words of length substring_length appearing in training sequences. 
+    '''
+    
+    unique_subsequences = set()
+
+    for string in training_sequences:
+        for start in range(len(string)-substring_length+1):
+            end = start + substring_length
+            substring = string[start:end]
+            unique_subsequences.add(substring)
+    
+    #Creating the word_2_index mapping words and their index. The keys are all the words.
+    unique_subsequences = sorted(unique_subsequences)
+    word_2_index = dict()
+    for idx, word in enumerate(unique_subsequences):
+        word_2_index[word] = idx
+        
+    return word_2_index
+
+def create_occ_feature(sequence, substring_length, dictionary, normalize=True):
+    '''Create the spectrum kernel feature vector of occurences of every word in dictionary/vocabulary.
+    
+    Input:
+        sequence: ADN sequence to transform
+        dictionary: already trained dictionary listing all the words appearing in training and their index
+        normalize: if true, transform the occurences in percentage (frequencies)
+    Ouput:
+        feature: occurence of each word in dictionary/vocabulary
+    '''
+    
+    feature = np.zeros(len(dictionary), dtype = int)
+    
+    for start in range(len(sequence)-substring_length+1):
+        end = start + substring_length
+        substring = sequence[start:end]
+        if substring in dictionary: #It is possible that some word in test are not appearing in training
+            feature[dictionary[substring]] = feature[dictionary[substring]] + 1
+            
+    if normalize:
+        feature = feature/feature.sum()
+        
+    return feature
+
+def spetrum_kernel(sequence_A, sequence_B, substring_length, dictionary, normalize=False):
+    '''substring_length-spectrum kernel
+    
+    Input:
+        sequence_A: first sequence
+        sequence_B: second sequence
+        substring_length: length of word in vocabulary
+        dictionary: vocabulary derived from training
+        normalize: if true, transform the occurences in percentage (frequencies)
+    Output:
+        kernel similarity between sequence_A and sequence_B
+    '''
+    
+    feature_A = create_occ_feature(sequence_A, substring_length, dictionary, normalize)
+    feature_B = create_occ_feature(sequence_B, substring_length, dictionary, normalize)
+    
+    return np.dot(feature_A, feature_B)
